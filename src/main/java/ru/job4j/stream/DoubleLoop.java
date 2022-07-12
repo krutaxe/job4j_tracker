560ff92a29a3360532af0ba21f8da50287c595b9
krutaxe <krutaxe@mail.ru>
2022-07-12 15:13:18 +0300

    5. Генерирование колоды карт. FlatMap [#504873 #321066]

--- src/main/java/ru/job4j/stream/DoubleLoop.java
+++ src/main/java/ru/job4j/stream/DoubleLoop.java
@@ -4,12 +4,9 @@ import java.util.stream.Stream;
 
 public class DoubleLoop {
     public static void main(String[] args) {
-        String[] levels = {"level 1", "level 2", "level 3"};
-        String[] tasks = {"task 1", "task 2", "task 3", "task 4"};
-
-        Stream.of(levels)
-                .flatMap(level -> Stream.of(tasks)
-                .map(task -> level + " " + task))
+        Stream.of(Card.Suit.values())
+                .flatMap(suit -> Stream.of(Card.Value.values())
+                .map(task -> new Card(suit, task)))
                 .forEach(System.out::println);
 
     }
