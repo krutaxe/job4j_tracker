a7ffcb86e953185c5ce960980415a8de17aae7ae
krutaxe <krutaxe@mail.ru>
2022-08-08 16:27:07 +0300

    4. Аттестация [#504878 #329749]

--- src/main/java/ru/job4j/mapa/AnalyzeByMap.java
+++ src/main/java/ru/job4j/mapa/AnalyzeByMap.java
@@ -6,45 +6,35 @@ public class AnalyzeByMap {
     public static double averageScore(List<Pupil> pupils) {
         int count = 0;
         double rsl = 0;
-        for (int i = 0; i < pupils.size(); i++) {
-            for (int j = 0; j < pupils.get(i).subjects().size(); j++) {
-                rsl += pupils.get(i).subjects().get(j).score();
+        for (Pupil pupil : pupils) {
+            for (Subject subject : pupil.subjects()) {
+                rsl += subject.score();
                 count++;
             }
         }
-        rsl = rsl / count;
-        return rsl;
+        return rsl / count;
     }
 
     public static List<Label> averageScoreByPupil(List<Pupil> pupils) {
         List<Label> list = new ArrayList<>();
         int count = 0;
         double score = 0;
-        for (int i = 0; i < pupils.size(); i++) {
-            for (int j = 0; j < pupils.get(i).subjects().size(); j++) {
-                score += pupils.get(i).subjects().get(j).score();
+        for (Pupil pupil : pupils) {
+            for (Subject subject : pupil.subjects()) {
+                score += subject.score();
                 count++;
-                if (count == pupils.get(i).subjects().size()) {
-                    list.add(new Label(pupils.get(i).name(), score / count));
-                    count = 0;
-                    score = 0;
-                }
             }
+            list.add(new Label(pupil.name(), score / count));
+            count = 0;
+            score = 0;
         }
         return list;
     }
 
     public static List<Label> averageScoreBySubject(List<Pupil> pupils) {
-        double sumScore = 0;
         Map<String, Double> map = new LinkedHashMap<>();
         List<Label> labelList = new ArrayList<>();
-        for (int i = 0; i < pupils.size(); i++) {
-            for (int j = 0; j < pupils.get(i).subjects().size(); j++) {
-                sumScore += pupils.get(j).subjects().get(i).score();
-            }
-            map.put(pupils.get(i).subjects().get(i).name(), sumScore);
-            sumScore = 0;
-        }
+        loadMap(map, pupils);
         for (Map.Entry<String, Double> value : map.entrySet()) {
             labelList.add(new Label(value.getKey(), value.getValue() / pupils.size()));
         }
@@ -53,13 +43,12 @@ public class AnalyzeByMap {
 
     public static Label bestStudent(List<Pupil> pupils) {
         List<Label> list = new ArrayList<>();
-        int count = 0;
         double score = 0;
-        for (int i = 0; i < pupils.size(); i++) {
-            for (int j = 0; j < pupils.get(i).subjects().size(); j++) {
-                score += pupils.get(i).subjects().get(j).score();
+        for (Pupil pupil : pupils) {
+            for (Subject subject : pupil.subjects()) {
+                score += subject.score();
             }
-            list.add(new Label(pupils.get(i).name(), score));
+            list.add(new Label(pupil.name(), score));
             score = 0;
         }
         list.sort(Label::compareTo);
@@ -67,20 +56,25 @@ public class AnalyzeByMap {
     }
 
     public static Label bestSubject(List<Pupil> pupils) {
-        double sumScore = 0;
         Map<String, Double> map = new LinkedHashMap<>();
         List<Label> labelList = new ArrayList<>();
-        for (int i = 0; i < pupils.size(); i++) {
-            for (int j = 0; j < pupils.get(i).subjects().size(); j++) {
-                sumScore += pupils.get(j).subjects().get(i).score();
-            }
-            map.put(pupils.get(i).subjects().get(i).name(), sumScore);
-            sumScore = 0;
-        }
+        loadMap(map, pupils);
+        System.out.println(map);
         for (Map.Entry<String, Double> value : map.entrySet()) {
             labelList.add(new Label(value.getKey(), value.getValue()));
         }
         labelList.sort(Label::compareTo);
         return labelList.get(labelList.size() - 1);
+    }
+
+    private static void loadMap(Map<String, Double> map, List<Pupil> pupils) {
+        double sum = 0;
+        for (int i = 0; i < pupils.size(); i++) {
+            for (int j = 0; j < pupils.get(i).subjects().size(); j++) {
+                sum += pupils.get(j).subjects().get(i).score();
+            }
+            map.put(pupils.get(i).subjects().get(i).name(), sum);
+            sum = 0;
+        }
     }
 }
