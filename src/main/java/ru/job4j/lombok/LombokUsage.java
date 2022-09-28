a5a3d256371b7511cfd987dfead43b40aff57a23
krutaxe <krutaxe@mail.ru>
2022-09-28 07:00:54 +0300

    2. @Setter, @Getter и другие [#504889]

--- src/main/java/ru/job4j/lombok/LombokUsage.java
+++ src/main/java/ru/job4j/lombok/LombokUsage.java
@@ -7,5 +7,24 @@ public class LombokUsage {
         bird.setColor("red");
         System.out.println(bird.getColor());
         System.out.println(bird);
+
+        var role = Role.of()
+                .id(1)
+                .name("ADMIN")
+                .accessBy("create")
+                .accessBy("update")
+                .accessBy("read")
+                .accessBy("delete")
+                .build();
+        System.out.println(role);
+
+        var permission = Permission.of()
+                .id(2)
+                .name("User")
+                .rules("user")
+                .rules("admin")
+                .rules("public")
+                .build();
+        System.out.println(permission);
     }
 }
