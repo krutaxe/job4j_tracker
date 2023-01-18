7d3aff7269bdc97a59fc39a22102e67ed7748740
krutaxe <krutaxe@mail.ru>
2023-01-18 19:46:37 +0300

    9. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,28 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    private static final String NEW_VALUE = "";
+
+    private int sizeOfEmpty = 10;
+
+    private String surname;
+
+    private String name;
+
+    public Broken() {
+    }
+
+    public void echo() {
+
+    }
+
+    public void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    public void method(int... h) {
+
+    }
+}
